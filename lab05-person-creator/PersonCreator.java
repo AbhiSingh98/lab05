25f2070dbee562c83155793ca15f8ed6ef3bc840
ABHIJIT SINGH <abhijitsingh@adelphi.internal>
2016-09-13 12:07:22 -0400

    lab05 almost done

--- lab05-person-creator/PersonCreator.java
+++ lab05-person-creator/PersonCreator.java
@@ -8,4 +8,51 @@
 public class PersonCreator
 {
     // Your class begins here!
+    private String name;
+    private int age;
+    private String sex;
+    private double height;
+    private double weight;
+    private int social;
+    private String address;
+    private int phoneNum;
+    private int dob;
+
+    public PersonCreator(String first, String last)
+    {
+        name = first + " " + last;
+
+    }
+
+    public void setName(String fullName)
+    {
+        name = fullName;   
+
+    }
+
+    public void setAge(int newAge)
+    {
+        age = newAge;
+
+    }
+
+    public void setSex(String newSex)
+    {
+        sex = newSex;
+    }
+
+    public void setHeight(double newHeight)
+    {
+        height = newHeight;
+    }
+
+    public void setWeight(double newWeight)
+    {
+        weight = newWeight;
+    }
+
+   
+   
+
 }
+
